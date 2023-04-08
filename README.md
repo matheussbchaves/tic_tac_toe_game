3cf99e04d9dd450cd9e34df86bd9e60e104b017a
matheussbchaves <matheussbchaves@gmail.com>
2023-04-08 14:08:51 -0300

    Add README.md and minor changes.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Tic Tac Toe Game
+
+Building a tic tac toe game using python and tkinter. Work in progress.
--- script/tic_tac_toe.py
+++ script/tic_tac_toe.py
@@ -8,41 +8,52 @@ class TicTacToeBoard(Tk):
     def __init__(self):
         super().__init__()
         self.title("Tic Tac Toe Game")
+        self.geometry(self._center_window())
+        self.resizable(False, False)
         self._cells = {}
         self._draw_board_display()
         self._draw_grid()
 
     def _draw_board_display(self):
         display_frame = Frame(self)
-        display_frame.pack(fill=X)
         self.display = Label(display_frame,
                              text='Ready?',
-                             font=font.Font(size=28, weight='bold'))
+                             font=font.Font(size=20, weight='bold'))
+        display_frame.pack()
         self.display.pack()
 
     def _draw_grid(self):
         grid_frame = Frame(self)
-        grid_frame.pack()
 
         for row in range(3):
-            self.rowconfigure(row, weight=1, minsize=50)
-            self.columnconfigure(row, weight=1, minsize=75)
-
             for col in range(3):
                 button = Button(grid_frame,
                                 text='',
                                 #font=font.Font(size=36, weight='bold'),
                                 #fg='black',
-                                width=3)
+                                width=3,
                                 #height=2,
-                                #highlightbackground='lightblue')
+                                #highlightbackground='lightblue'
+                                )
                 
                 self._cells[button] = (row, col)
                 button.grid(row=row,
                             column=col,
-                            padx=5,
-                            pady=5,
+                            ipadx=25,
+                            ipady=25,
                             sticky='nsew')
+        
+        grid_frame.pack()
+                
+    def _center_window(self):
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+
+        center_width = int(screen_width / 2 - 300 / 2)
+        center_height = int(screen_height / 2 - 300 / 2)
+
+        return f'300x300+{center_width}+{center_height}'
+    
 
 
 def main():
